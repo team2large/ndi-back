68320136deb4fe9610627089bbbbb2a26c5629e3
celian-rib <celian.riboulet@gmail.com>
2022-11-24 11:50:31 +0100

    Counter and test auto migrate

--- src/routes/template.route.ts
+++ src/routes/template.route.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from 'express';
+import client from '../prisma';
 import app from '../app';
 
 export default function templateRoutes() {
 
   app.get('/', async (req: Request, res: Response) => {
-    res.json('Ndi backend');
+    
+    // On ajoute une "visite" du site dans la base de données
+    await client.siteVisits.create({ 
+        data :{ ipAddress: req.ip } 
+    });
+
+    // On récupère le nombre de visites du site
+    const visits = await client.siteVisits.count();
+
+    res.status(200).json(`${visits} visits`);
   });
 
-  app.get('/helloJson', async (req: Request, res: Response) => {
-    res.json({
-        message: 'Hello world',
-    });
-  });
 }
