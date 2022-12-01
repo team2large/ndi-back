9dd35573bba21154a2e08281daa1500598fc9671
Tomm Jobit <tjobit@MBATomm.local>
2022-12-01 22:47:33 +0100

    remove visits table

--- prisma/migrations/20221201214625_base_db/migration.sql
+++ prisma/migrations/20221201214625_base_db/migration.sql
@@ -1,13 +1,4 @@
 -- CreateTable
-CREATE TABLE "siteVisits" (
-    "id" SERIAL NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ipAddress" TEXT NOT NULL,
-
-    CONSTRAINT "siteVisits_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Game" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as dotenv from 'dotenv';
 import { startApp } from './app';
-import templateRoutes from './routes/template.route';
 import userLeaderboardRoutes from './routes/userLeaderboard.routes';
 import gameRoutes from './routes/game.routes';
 
 dotenv.config();
 
 startApp();
-templateRoutes();
 userLeaderboardRoutes();
 gameRoutes();
